a4fdd7ee8ced5c4cdbbbd30cacf47dc573efcfd8
Johnny <ramirezjohnny@icloud.com>
2024-04-09 13:44:37 -0400

    make progress on calendar

--- backend/server.py
+++ backend/server.py
@@ -38,26 +38,26 @@ def logout():
 def announcements():
     res = get_records('announcement')
     print('announcements:', res)
-    return {'data': res}
+    return {'announcements': res}
 
 @app.route('/api/clubs')
 def clubs():
     res = get_records('club')
     # res = get_clubs()
     print('clubs', res)
-    return {'data': res}
+    return {'clubs': res}
 
 @app.route('/api/events')
 def events():
     res = get_records('event')
     print('events:', res)
-    return {'data': res}
+    return {'events': res}
 
 @app.route('/api/users')
 def users():
     res = get_records('user')
     print('users:', res)
-    return {'data': res}
+    return {'users': res}
 
 #-----------------------------------------------------------------------
 @app.route('/', methods=['GET'])
@@ -74,7 +74,34 @@ def contact_page():
 
 @app.route('/events', methods=['GET'])
 def events_page():
-    return render_template('pages/calendarpage.html')
+    fetched_events = events()
+    fetched_events = list(map(lambda event: {
+            'name': event[1], 
+            'location': event[2],
+            'description': event[3], 
+            'start_time': event[4],
+            'end_time': event[4]
+        }, fetched_events['events']))
+    sunday_event = list(filter(lambda event: "Sun" in event['start_time'], fetched_events))
+    monday_events = list(filter(lambda event: "Mon" in event['start_time'], fetched_events))
+    tuesday_events = list(filter(lambda event: "Tue" in event['start_time'], fetched_events))
+    wednesday_events = list(filter(lambda event: "Wed" in event['start_time'], fetched_events))
+    thursday_events = list(filter(lambda event: "Thu" in event['start_time'], fetched_events))
+    friday_events = list(filter(lambda event: "Fri" in event['start_time'], fetched_events))
+    saturday_events = list(filter(lambda event: "Sat" in event['start_time'], fetched_events))
+
+    print('MONDAY EVENTS',  monday_events)
+
+
+    return render_template(
+        'pages/calendarpage.html',
+        monday_events=monday_events,
+        tuesday_events=tuesday_events,
+        wednesday_events=wednesday_events,
+        thursday_events=thursday_events,
+        friday_events=friday_events,
+        saturday_events=saturday_events
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
