0f5c2e666ab149a27cc983b307efaf4a269dfa33
Ella Colby <ellacolby@princeton.edu>
2024-04-22 12:10:59 -0400

    colors in cal page and standardizing club naming

--- backend/name.py
+++ backend/name.py
@@ -8,8 +8,12 @@ def get_email(uid):
     req_lib = ReqLib()
     result = req_lib.getJSON('/users', uid=uid)
     return result[0]['mail']
+def get_all(uid):
+    req_lib = ReqLib()
+    result = req_lib.getJSON('/users', uid=uid)
+    return result[0]
 
 if __name__ == "__main__":
     uid = 'ec9834'
-    user_info = get_email(uid)
+    user_info = get_all(uid)
     print(user_info)
